816b5fd4f0374e309c8f4eaa944f61112a3a45ad
BrunoAstori <brunoastori0@gmail.com>
2026-06-14 12:39:40 -0300

    probando

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors({
-    origin: 'https://tp-integrador-dds.onrender.com:5173', 
+    origin: 'https://tp-integrador-dds.onrender.com', 
     credentials: true 
 }));
 
