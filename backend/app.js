e746fbbce6b1a8ddc5808d70e573fce9059a6f25
BrunoAstori <brunoastori0@gmail.com>
2026-06-14 16:35:49 -0300

    arreglando

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors({
-    origin: 'https://tp-integrador-dds.onrender.com', 
+    origin: 'http://localhost:5173', 
     credentials: true 
 }));
 
